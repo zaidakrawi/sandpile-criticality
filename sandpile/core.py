9307335852f07f63addae0e7b23bbd1719fc0c7a
Zaid Akrawi <zaidakrawi4@gmail.com>
2025-06-19 22:58:02 +0200

    Added animation method to Sandpile class

--- sandpile/core.py
+++ sandpile/core.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 # Sandpile class with a 2D grid and grain adding method
@@ -41,4 +42,31 @@ class Sandpile:
         plt.imshow(self.grid, cmap="plasma", interpolation="nearest")
         plt.colorbar(label="Number of grains")
         plt.title("Sandpile Configuration")
+        plt.show()
+
+    def animate(self, num_frames=200, drop_location=None):
+        """
+        Animation of sanpile
+        """
+        fig, ax = plt.subplots()
+        im = ax.imshow(self.grid, cmap='viridis', vmin=0, vmax=4)
+        plt.colorbar(im, ax=ax, label='Number of grains')
+
+        def update(frame):
+            # Drop grain at fixed or random location
+            if drop_location:
+                x, y = drop_location
+            else:
+                x = np.random.randint(0, self.size)
+                y = np.random.randint(0, self.size)
+            self.add_grain(x,y)
+            self.topple()
+            im.set_array(self.grid)
+            return [im]
+        
+        ani = animation.FuncAnimation(
+            fig, update, frames=num_frames, interval=50, blit=True
+        )
+
+        plt.title("Sandpile Animation")
         plt.show()
--- scripts/simulation.py
+++ scripts/simulation.py
@@ -1,11 +1,15 @@
 from sandpile.core import Sandpile
 
 def main():
-    pile = Sandpile(size=20)
+    # pile = Sandpile(size=20)
     
-    # Drop grains at the center
-    for _ in range(1000):
-        pile.add_grain(10, 10)
-        pile.topple()
+    # # Drop grains at the center
+    # for _ in range(10000):
+    #     pile.add_grain(10, 10)
+    #     pile.topple()
 
-    pile.visualize()
+    # pile.visualize()
+
+
+    pile = Sandpile(size=20)
+    pile.animate(num_frames=300, drop_location=(10, 10))
